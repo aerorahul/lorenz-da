b80bc6e115c534c55a1319dce360a79a14933e5e
rmahajan <rmahajan@989189f8-c1fd-0bc3-bebe-85be595008c9>
2012-03-15 18:09:08 +0000

    src/L96_stats.py added the ability to make B using 3 methods: NMC, climatology and from EnKF output

--- src/L96_stats.py
+++ src/L96_stats.py
@@ -13,70 +13,131 @@
 #                the 1998 Lorenz & Emanuel system
 ###############################################################
 
+###############################################################
 __author__    = "Rahul Mahajan"
 __email__     = "rahul.mahajan@nasa.gov"
 __copyright__ = "Copyright 2012, NASA / GSFC / GMAO"
 __license__   = "GPL"
 __status__    = "Prototype"
+###############################################################
 
+###############################################################
 import os
 import sys
 import numpy         as     np
 from   netCDF4       import Dataset
 from   scipy         import integrate
 from   module_Lorenz import L96
+from   module_IO     import *
+###############################################################
 
-dt = 1.0e-4    # time-step
-ts = 10.0      # time for spin-up  (50 days)
-tf = 0.05      # time for forecast (6 hours)
-Ne = 500       # no. of samples to estimate B
-pscale = 0.001 # scale of perturbations to add
+###############################################################
+def main():
 
-# initial setup from LE1998
-Ndof  = 40
-F     = 8.0
-dF    = 0.0
-x0    = np.ones(Ndof) * F
-x0[0] = 1.001 * F
+    method = 'EnKF' # choose method to create B: NMC / Climo / EnKF
 
-# get a state on the attractor
-print 'spinning-up onto the attractor ...'
-ts = np.arange(0.0,ts+dt,dt)       # how long to run onto the attractor
-xs = integrate.odeint(L96, x0, ts, (F,0.0))
+    if ( (method == 'NMC') or (method == 'Climo') ):
 
-# use the end state as IC
-xt = xs[-1,:]
+        dt = 1.0e-4     # time-step
+        tf = 0.05       # time for forecast (6 hours)
+        ts = 50*4*tf    # time for spin-up  (50 days)
+        Ne = 500        # no. of samples to estimate B
 
-# allocate space upfront
-X = np.zeros((Ndof,Ne))
+        # initial setup from LE1998
+        Ndof  = 40
+        F     = 8.0
+        dF    = 0.0
+        x0    = np.ones(Ndof) * F
+        x0[0] = 1.001 * F
 
-print 'running ON the attractor ...'
+        if ( method == 'NMC' ):
+            pscale = 1.0e-3         # scale of perturbations to add
 
-tf0 = np.arange(0.0,1*tf+dt,dt)
-tf1 = np.arange(0.0,3*tf+dt,dt)
-tf2 = np.arange(0.0,4*tf+dt,dt)
+    elif ( method == 'EnKF' ):
 
-for i in range(0,Ne):
-    xs = integrate.odeint(L96, xt, tf0, (F+dF,0.0))
-    xt = xs[-1,:].copy()
+        # get the name of output diagnostic file to read
+        [fname] = get_input_arguments()
+        if ( not os.path.isfile(fname) ):
+            print '%s does not exist' % fname
+            sys.exit(1)
 
-    xs = integrate.odeint(L96, xt, tf1, (F+dF,0.0))
-    x24 = xs[-1,:].copy()
+    if ( (method == 'NMC') or (method == 'Climo') ):
 
-    xs = integrate.odeint(L96, x24, tf2, (F+dF,0.0))
-    x48 = xs[-1,:].copy()
+        # get a state on the attractor
+        print 'spinning-up onto the attractor ...'
+        ts = np.arange(0.0,ts+dt,dt)       # how long to run onto the attractor
+        xs = integrate.odeint(L96, x0, ts, (F,0.0))
 
-    X[:,i] = x48 - x24
+        # use the end state as IC
+        xt = xs[-1,:]
 
-    xt = xt + pscale * np.random.randn(Ndof)
+        print 'Using the NMC method to create B'
 
-# compute climatological covariance matrix
-B = np.dot(X,np.transpose(X)) / (Ne - 1)
+        # allocate space upfront
+        X = np.zeros((Ndof,Ne))
 
-# save B to disk for use with DA experiments
-print 'save B to disk ...'
-nc       = Dataset('L96_climo_B.nc4',mode='w',clobber=True,format='NETCDF4')
-Dim      = nc.createDimension('xyz',Ndof)
-Var      = nc.createVariable('B', 'f8', ('xyz','xyz',))
-Var[:,:] = B
-nc.close()
+        print 'running ON the attractor ...'
+
+        tf0 = np.arange(0.0,1*tf+dt,dt)
+        tf1 = np.arange(0.0,3*tf+dt,dt)
+        tf2 = np.arange(0.0,4*tf+dt,dt)
+
+        for i in range(0,Ne):
+            xs = integrate.odeint(L96, xt, tf0, (F+dF,0.0))
+            xt = xs[-1,:].copy()
+
+            xs = integrate.odeint(L96, xt, tf1, (F+dF,0.0))
+            x24 = xs[-1,:].copy()
+
+            xs = integrate.odeint(L96, x24, tf2, (F+dF,0.0))
+            x48 = xs[-1,:].copy()
+
+            X[:,i] = x48 - x24
+
+            xt = xt + pscale * np.random.randn(Ndof)
+
+        # compute climatological covariance matrix
+        B = np.dot(X,np.transpose(X)) / (Ne - 1)
+
+    elif ( method == 'EnKF'):
+
+        print 'Using the EnKF output to create B'
+
+        try:
+            nc   = Dataset(fname, mode='r', format='NETCDF4')
+            Ndof = len(nc.dimensions['ndof'])
+            Nens = len(nc.dimensions['ncopy'])
+            Ntim = len(nc.dimensions['ntime'])
+            Xb   = np.transpose(np.squeeze(nc.variables['prior'][:,]),(0,2,1))
+            xbm  = np.mean(Xb, axis=2)
+            nc.close()
+        except Exception as Instance:
+            print 'Exception occurred during read of ' + fname
+            print type(Instance)
+            print Instance.args
+            print Instance
+            sys.exit(1)
+
+        Bi = np.zeros((Ntim,Ndof,Ndof))
+        for i in range(0,Ntim):
+            Xbp = np.transpose(np.transpose(np.squeeze(Xb[i,])) - np.squeeze(xbm[i,]))
+            Bi[i,] = np.dot(Xbp, np.transpose(Xbp)) / (Nens - 1)
+
+        B = np.mean(Bi[100:,],axis=0)
+        print np.diag(B)
+
+    # save B to disk for use with DA experiments
+    print 'save B to disk ...'
+    nc       = Dataset('L96_climo_B_' + method +'.nc4',mode='w',clobber=True,format='NETCDF4')
+    Dim      = nc.createDimension('xyz',Ndof)
+    Var      = nc.createVariable('B', 'f8', ('xyz','xyz',))
+    Var[:,:] = B
+    nc.close()
+
+    sys.exit(0)
+###############################################################
+
+###############################################################
+if __name__ == "__main__":
+	main()
+###############################################################

3eac198318f307735f7330ee3c6a5286f5492f0e
rmahajan <rmahajan@989189f8-c1fd-0bc3-bebe-85be595008c9>
2012-03-28 21:32:30 +0000

    linspace is used instead of arange from numpy. arange is buggy. other than that, use plot_L96, instead of plot_trace; much better to visualize.

--- src/L96_model.py
+++ src/L96_model.py
@@ -9,7 +9,8 @@
 ###############################################################
 
 ###############################################################
-# L96_model.py - integrate the 1998 Lorenz and Emanuel model
+# L96_model.py - integrate the 1998 Lorenz and Emanuel model,
+#                test its TLM and adjoint
 ###############################################################
 
 __author__    = "Rahul Mahajan"
@@ -18,66 +19,67 @@ __copyright__ = "Copyright 2012, NASA / GSFC / GMAO"
 __license__   = "GPL"
 __status__    = "Prototype"
 
-import numpy      as     np
-from   lorenz     import L96, L96_tlm
-from   plot_stats import plot_trace
-from   scipy      import integrate
-from   matplotlib import pyplot
+import numpy         as     np
+from   scipy         import integrate
+from   matplotlib    import pyplot
+from   plot_stats    import *
+from   module_Lorenz import *
 
 # insure the same sequence of random numbers EVERY TIME
 np.random.seed(0)
 
-dt = 1.0e-4
-t0 = 0.0
-
-tol = 1.0e-12
+model      = type('',(),{})  # model Class
+model.Name = 'L96'           # model name
+model.Ndof = 40              # model degrees of freedom
+model.Par  = [8.0, 0.4]      # model parameters F, dF
 
 # initial setup from LE1998
-Ndof  = 40
-F     = 8.0
-x0    = np.ones(Ndof) * F
-x0[0] = 1.001 * F
+x0    = np.ones(model.Ndof) * model.Par[0]
+x0[0] = 1.001 * model.Par[0]
 
-label_list = []
-for j in range(1,Ndof+1): label_list.append( 'x' + str(j) )
+dt  = 1.0e-4
+t0  = 0.0
+tol = 1.0e-13
+nf  = 4.0
 
 print 'spinning-up ON the attractor ...'
+print '--------------------------------'
 
 tf = 25.0
-ts = np.arange(t0,tf+dt,dt)
+ts = np.linspace(t0,tf,tf/dt+1.0,endpoint=True)
 
-xs = integrate.odeint(L96, x0, ts, (F,0.0))
+xs = integrate.odeint(L96, x0, ts, (model.Par[0],0.0))
+plot_L96(ver=xs[-1,:],obs=xs[-1,:],t=tf,N=model.Ndof)
 
-plot_trace(ver = np.transpose(xs), label=label_list, N=3)
-
-# let final state of previous integration be IC of next integration
 x0 = xs[-1,:].copy()
 
-tf = 0.05 * 4
-ts = np.arange(t0,tf+dt,dt)
+tf = 0.05 * nf
+ts = np.linspace(t0,tf,tf/dt+1.0,endpoint=True)
 
-xs = integrate.odeint(L96, x0, ts, (F,0.0),rtol=tol,atol=tol)
+xs = integrate.odeint(L96, x0, ts, (model.Par[0],0.0),rtol=tol,atol=tol)
 xsf = xs[-1,:].copy()
 
-xp0 = np.random.randn(Ndof) * 1.0e-4
+xp0 = np.random.randn(model.Ndof) * 1.0e-4
 
-xsp = integrate.odeint(L96, x0+xp0, ts, (F,0.0),rtol=tol,atol=tol)
+xsp = integrate.odeint(L96, x0+xp0, ts, (model.Par[0],0.0),rtol=tol,atol=tol)
 xspf = xsp[-1,:].copy()
 
-xp = integrate.odeint(L96_tlm, xp0, ts, (F, xs, ts, False),rtol=tol,atol=tol)
+xp = integrate.odeint(L96_tlm, xp0, ts, (model.Par[0], xs, ts, False),rtol=tol,atol=tol)
 xpf = xp[-1,:].copy()
 
-print 'check TLM ...'
-for j in range(0,Ndof):
-    print 'j = %2d | Ratio = %f' % (j+1, ( xspf[j] - xsf[j] ) / xpf[j])
+print 'check TLM ..'
+for j in range(0,model.Ndof):
+    print 'j = %2d | Ratio = %14.13f' % (j+1, ( xspf[j] - xsf[j] ) / xpf[j])
+print '--------------------------------'
 
 xa0 = xpf.copy()
-xa = integrate.odeint(L96_tlm, xa0, ts, (F, np.flipud(xs), ts, True),rtol=tol,atol=tol)
+xa = integrate.odeint(L96_tlm, xa0, ts, (model.Par[0], np.flipud(xs), ts, True),rtol=tol,atol=tol)
 xaf = xa[-1,:].copy()
 
 q1 = np.dot(np.transpose(xpf),xpf)
 q2 = np.dot(np.transpose(xaf),xp0)
 
-print 'check adjoint ... %f' % (q2 -q1)
+print 'check adjoint .. %14.13f' % (q2 -q1)
+print '--------------------------------'
 
 pyplot.show()

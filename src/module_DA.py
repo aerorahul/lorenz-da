module_DA.py observational increment for incremental 4Dvar had a sign error. fixed. incremental 4dvar with 1 outer loop is now verrrrrrrry similar to the vanilla 4dvar. this is tested for 50 assimilation cycles.

src/module_DA.py
@@ -733,7 +733,7 @@ minimization - minimization class
 
         # get observational increment at all observation times
         for i in range(0,fdvar.nobstimes):
-            d[i,:] = np.dot(H,xnl[fdvar.twind_obsIndex[i],:]) - y[i,:]
+            d[i,:] = y[i,:] - np.dot(H,xnl[fdvar.twind_obsIndex[i],:])
 
         while ( np.abs(Jold - J) > minimization.tol ):
 